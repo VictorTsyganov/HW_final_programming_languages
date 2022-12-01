f15ef6fd2465e3f1f9f5f8cb8fe5766ddeb17a1c
Victor Tsyganov <tsyganov1989@gmail.com>
2022-12-01 11:39:46 +0300

    add input and print massiv to program

---
+++ final_project/Program.cs
@@ -0,0 +1,25 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на стартевыполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+
+Console.Clear();
+
+string[] Array = NewArray();
+Console.WriteLine();
+PrintArray(Array);
+
+string[] NewArray()
+{
+    Console.Write("Введите элементы массива через пробел: ");
+    string[] arr = Console.ReadLine().Split();
+    return arr;
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
